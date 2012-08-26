dd83709043b4863c2c18e11b1e9b2c99adbcdb0c
Jean-Christophe DUBOIS <jcd@tribudubois.net>
2012-08-26 04:11:34 +0200

    Change license to LGPL 2.1
    This is a library after all.
    
    Signed-off-by: Jean-Christophe DUBOIS <jcd@tribudubois.net>

--- clean_malloc.c
+++ clean_malloc.c
@@ -3,8 +3,8 @@
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
+ * it under the terms of the GNU Lesset General Public License as published 
+ * by the Free Software Foundation; either version 2.1, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
